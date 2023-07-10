90f674feb6d907327652a2046e08ed849cded023
hsh814 <hsh814@naver.com>
2023-07-10 18:19:36 +0900

    Add meta-test.py

---
+++ scripts/meta-test.py
@@ -0,0 +1,245 @@
+#! /usr/bin/env python3
+import os
+import sys
+from typing import List, Set, Dict, Tuple
+import json
+import subprocess
+import argparse
+import patch
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+class ConfigFiles:
+  root_dir: str
+  meta_data: str
+  repair_conf: str
+  meta_program: str
+  patch_dir: str
+  work_dir: str
+  project_dir: str
+  meta_patch_obj_file: str
+  benchmark: str
+  subject: str
+  bid: str
+  out_base_dir: str
+  out_dir: str
+  snapshot_dir: str
+  snapshot_file: str
+  
+  def __init__(self):
+    self.root_dir = ROOT_DIR
+    self.meta_data = os.path.join(self.root_dir, "patches", "meta-data.json")
+  def set(self, bug_info: dict):
+    patches_dir = os.path.join(self.root_dir, "patches")
+    self.bid = bug_info["bug_id"]
+    self.benchmark = bug_info["benchmark"]
+    self.subject = bug_info["subject"]
+    self.project_dir = os.path.join(patches_dir, self.benchmark, self.subject, self.bid)
+    self.work_dir = os.path.join(self.project_dir, "patched")
+    self.repair_conf = os.path.join(self.project_dir, "repair.conf")
+    self.meta_program = os.path.join(self.project_dir, "meta-program.json")
+    patch.compile(os.path.join(self.project_dir, "concrete"))
+    self.meta_patch_obj_file = os.path.join(self.project_dir, "concrete", "libuni_klee_runtime.bca")
+  def set_out_dir(self, out_dir: str, out_dir_prefix: str, bug_info: dict):
+    if out_dir == "":
+      self.out_base_dir = os.path.join(self.root_dir, "out", self.benchmark, self.subject, self.bid)
+    elif out_dir == "out":
+      self.out_base_dir = self.work_dir
+    else:
+      self.out_base_dir = out_dir
+    no = self.find_num(self.out_base_dir, out_dir_prefix)
+    self.out_dir = os.path.join(self.out_base_dir, f"{out_dir_prefix}-{no}")
+    self.snapshot_dir = os.path.join(self.out_base_dir, "snapshot")
+    if "snapshot" in bug_info:
+      self.snapshot_file = os.path.join(self.snapshot_dir, bug_info["snapshot"])
+    else:
+      self.snapshot_file = os.path.join(self.snapshot_dir, "snapshot-last.json")
+  def find_num(dir: str, name: str) -> int:
+    result = 0
+    dirs = os.listdir(dir)
+    while True:
+      if f"{name}-{result}" in dirs:
+        result += 1
+      else:
+        break
+    return result
+
+  def read_conf_file(self) -> dict:
+    with open(self.repair_conf, "r") as f:
+      lines = f.readlines()
+    result = dict()
+    for line in lines:
+      line = line.strip()
+      if len(line) == 0:
+        continue
+      if line.startswith("#"):
+        continue
+      key, value = line.split(":", 1)
+      result[key] = value
+    return result
+  def read_meta_data(self) -> dict:
+    with open(self.meta_data, "r") as f:
+      return json.load(f)
+  def read_meta_program(self) -> dict:
+    with open(self.meta_program, "r") as f:
+      return json.load(f)
+
+class Config:
+  cmd: str
+  patch_ids: List[str]
+  addt_patch_ids: List[str]
+  meta: dict
+  bug_info: dict
+  meta_program: dict
+  query: str
+  debug: bool
+  outdir: str
+  workdir: str
+  project_conf: dict
+  conf_files: ConfigFiles
+  
+  def get_bug_info(self, bugid: str) -> dict:
+    def check_int(s: str) -> bool:
+      try:
+        int(s)
+        return True
+      except ValueError:
+        return False
+    num = -1
+    if check_int(bugid):
+      num = int(bugid)
+    for data in self.meta:
+      bid = data["bug_id"]
+      benchmark = data["benchmark"]
+      subject = data["subject"]
+      id = data["id"]
+      if num != -1:
+        if num == id:
+          return data
+      if bugid.lower() in bid.lower():
+        return data
+    return None
+  
+  def get_patch_ids(self, patch_ids: list) -> List[str]:
+    self.meta_program = self.conf_files.read_meta_program()
+    result = list()
+    for patch in self.meta_program["patches"]:
+      if str(patch["id"]) in patch_ids:
+        result.append(str(patch["id"]))
+      elif patch["name"] in patch_ids:
+        result.append(str(patch["id"]))
+    return result
+  
+  def parse_query(self, additional: str) -> Tuple[dict, list]:
+    parsed: List[str] = None
+    if ":" in self.query:
+      parsed = self.query.rsplit(":", 1)
+    else:
+      parsed = self.query.rsplit("/", 1)
+    bugid = parsed[0]
+    self.bug_info = self.get_bug_info(bugid)
+    if self.bug_info is None:
+      print(f"Cannot find patch for {self.query} - {bugid}")
+      sys.exit(1)
+    # Set config files
+    self.conf_files.set(self.bug_info)
+    # Set patch ids
+    patchid = "buggy"
+    if len(parsed) > 1:
+      patchid = parsed[1]
+    self.patch_ids = self.get_patch_ids(patchid.split(","))
+    if additional != "":
+      self.addt_patch_ids = self.get_patch_ids(additional.split(","))
+    else:
+      self.addt_patch_ids = list()
+    print(f"query: {self.query} => bugid {self.bug_info}, patchid {self.patch_ids}")
+    
+  def init(self, additional: str):
+    self.meta = self.conf_files.read_meta_data()
+    self.parse_query(additional)
+    self.project_conf = self.conf_files.read_conf_file()
+  
+  def __init__(self, cmd: str, query: str, debug: bool):
+    self.cmd = cmd
+    self.query = query    
+    self.debug = debug
+    self.conf_files = ConfigFiles()
+  
+  @staticmethod  
+  def parser(argv: List[str]) -> 'Config':
+    parser = argparse.ArgumentParser(description="Test script for uni-klee")
+    parser.add_argument("cmd", help="Command to execute", choices=["run", "cmp", "fork", "snapshot", "batch", "filter"])
+    parser.add_argument("query", help="Query to execute")
+    parser.add_argument("-a", "--additional", help="Additional arguments", default="")
+    parser.add_argument("-d", "--debug", help="Debug mode", action="store_true")
+    parser.add_argument("-o", "--outdir", help="Output directory", default="")
+    parser.add_argument("-p", "--outdir-prefix", help="Output directory prefix", default="uni-m-out")
+    args = parser.parse_args(argv[1:])
+    conf = Config(args.cmd, args.query, args.debug)
+    conf.init(args.additional)
+    conf.conf_files.set_out_dir(args.outdir, args.outdir_prefix, conf.meta)
+    return conf
+  
+  def append_snapshot_cmd(self, cmd: List[str]):
+    snapshot_dir = self.conf_files.snapshot_dir
+    patch_str = ",".join(self.patch_ids)
+    cmd.append(f"--output-dir={snapshot_dir}")
+    cmd.append(f"--patch-id={patch_str}")
+  
+  def append_cmd(self, cmd: List[str]):
+    out_dir = self.conf_files.out_dir
+    patch_str = ",".join(self.patch_ids)
+    default_opts = ["--no-exit-on-error", "--simplify-sym-indices", "--make-lazy"]
+    cmd.extend(default_opts)
+    cmd.append(f"--output-dir={out_dir}")
+    cmd.append(f"--patch-id={patch_str}")
+  
+  def get_cmd_opts(self, is_snapshot: bool) -> str:
+    target_function = self.bug_info["target_function"]
+    link_opt = f"--link-llvm-lib={self.conf_files.meta_patch_obj_file}"
+    result = ["uni-klee", "--libc=uclibc", "--posix-runtime", "--external-calls=all",
+              "--allocate-determ", "--write-smt2s", "--write-kqueries", "--log-trace",
+              f"--target-function={target_function}", link_opt]
+    if "klee_flags" in self.project_conf:
+      link_opt = f"--link-llvm-flags={self.project_conf['klee_flags']}"
+      result.append(link_opt)
+    if is_snapshot:
+      self.append_snapshot_cmd(result)
+    else:
+      self.append_cmd(result)
+    bin_file = os.path.basename(self.project_conf["binary_path"])
+    target = bin_file + ".bc"
+    result.append(target)
+    if "test_input_list" in self.project_conf:
+      poc_path = "exploit"
+      if "poc_path" in self.project_conf:
+        poc_path = self.project_conf["poc_path"]
+      target_cmd = self.project_conf["test_input_list"].replace("$POC", poc_path)
+      result.append(target_cmd)
+    return " ".join(result)
+
+class Runner:
+  config: Config
+  def __init__(self, conf: Config):
+    self.config = conf
+  def execute(self, cmd: str, dir: str, env: dict = None):
+    print(f"Change directory to {dir}")
+    print(f"Executing: {cmd}")
+    if env is None:
+      env = os.environ
+    proc = subprocess.run(cmd, shell=True, cwd=dir, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(f"Exit code: {proc.returncode}")
+    return proc.returncode
+  def run(self):
+    pass
+
+
+
+def main(args: List[str]):
+  root_dir = ROOT_DIR
+  os.chdir(root_dir)
+  conf = Config.parser(args)
+  runner = Runner(conf)
+  runner.run()
+
+if __name__ == "__main__":
+  main(sys.argv)
--- scripts/testm.py
+++ scripts/testm.py
@@ -4,8 +4,157 @@ import sys
 from typing import List, Set, Dict, Tuple
 import json
 import subprocess
-
+import argparse
 import patch
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+class ConfigFiles:
+  root_dir: str
+  meta_data: str
+  repair_conf: str
+  meta_program: str
+  patch_dir: str
+  work_dir: str
+  project_dir: str
+  meta_patch_obj_file: str
+  benchmark: str
+  subject: str
+  bid: str
+  out_base_dir: str
+  out_dir: str
+  
+  def __init__(self):
+    self.root_dir = ROOT_DIR
+    self.meta_data = os.path.join(self.root_dir, "patches", "meta-data.json")
+  def set(self, bug_info: dict):
+    patches_dir = os.path.join(self.root_dir, "patches")
+    self.bid = bug_info["bug_id"]
+    self.benchmark = bug_info["benchmark"]
+    self.subject = bug_info["subject"]
+    self.project_dir = os.path.join(patches_dir, self.benchmark, self.subject, self.bid)
+    self.work_dir = os.path.join(self.project_dir, "patched")
+    self.repair_conf = os.path.join(self.project_dir, "repair.conf")
+    self.meta_program = os.path.join(self.project_dir, "meta-program.json")
+    self.meta_patch_obj_file = os.path.join(self.project_dir, "concrete", "libuni_klee_runtime.bca")
+  def set_out_dir(self, out_dir: str, out_dir_prefix: str):
+    if out_dir == "":
+      self.out_base_dir = os.path.join(self.root_dir, "out", self.benchmark, self.subject, self.bid)
+      self.out_dir = os.path.join(self.out_base_dir)
+    elif out_dir == "out":
+      self.out_base_dir = self.work_dir
+    else:
+      self.out_base_dir = out_dir
+  
+  
+  def read_conf_file(self) -> dict:
+    with open(self.repair_conf, "r") as f:
+      lines = f.readlines()
+    result = dict()
+    for line in lines:
+      line = line.strip()
+      if len(line) == 0:
+        continue
+      if line.startswith("#"):
+        continue
+      key, value = line.split(":", 1)
+      result[key] = value
+    return result
+  def read_meta_data(self) -> dict:
+    with open(self.meta_data, "r") as f:
+      return json.load(f)
+  def read_meta_program(self) -> dict:
+    with open(self.meta_program, "r") as f:
+      return json.load(f)
+
+class Config:
+  cmd: str
+  patch_ids: List[str]
+  meta: dict
+  bug_info: dict
+  query: str
+  debug: bool
+  outdir: str
+  workdir: str
+  project_conf: dict
+  conf_files: ConfigFiles
+  
+  def get_bug_info(self, bugid: str) -> dict:
+    def check_int(s: str) -> bool:
+      try:
+        int(s)
+        return True
+      except ValueError:
+        return False
+    num = -1
+    if check_int(bugid):
+      num = int(bugid)
+    for data in self.meta:
+      bid = data["bug_id"]
+      benchmark = data["benchmark"]
+      subject = data["subject"]
+      id = data["id"]
+      if num != -1:
+        if num == id:
+          return data
+      if bugid.lower() in bid.lower():
+        return data
+    return None
+  
+  def get_patch_ids(self, patch_ids: list) -> List[str]:
+    meta_program = self.conf_files.read_meta_program()
+    result = list()
+    for patch in meta_program["patches"]:
+      if str(patch["id"]) in patch_ids:
+        result.append(str(patch["id"]))
+      elif patch["name"] in patch_ids:
+        result.append(str(patch["id"]))
+    return result
+  
+  def parse_query(self) -> Tuple[dict, list]:
+    parsed: List[str] = None
+    if ":" in self.query:
+      parsed = self.query.rsplit(":", 1)
+    else:
+      parsed = self.query.rsplit("/", 1)
+    bugid = parsed[0]
+    self.bug_info = self.get_bug_info(bugid)
+    if self.bug_info is None:
+      print(f"Cannot find patch for {self.query} - {bugid}")
+      sys.exit(1)
+    # Set config files
+    self.conf_files.set(self.bug_info)
+    # Set patch ids
+    patchid = "buggy"
+    if len(parsed) > 1:
+      patchid = parsed[1]
+    self.patch_ids = self.get_patch_ids(patchid.split(","))
+    print(f"query: {self.query} => bugid {self.bug_info}, patchid {self.patch_ids}")
+    
+  def init(self):
+    self.meta = self.conf_files.read_meta_data()
+    self.parse_query()
+  
+  def __init__(self, cmd: str, query: str, debug: bool):
+    self.cmd = cmd
+    self.query = query    
+    self.debug = debug
+    self.conf_files = ConfigFiles()
+  
+  @staticmethod  
+  def parser(argv: List[str]) -> 'Config':
+    parser = argparse.ArgumentParser(description="Test script for uni-klee")
+    parser.add_argument("cmd", help="Command to execute", choices=["run", "cmp", "fork", "snapshot", "batch", "filter"])
+    parser.add_argument("query", help="Query to execute")
+    parser.add_argument("-a", "--additional", help="Additional arguments")
+    parser.add_argument("-d", "--debug", help="Debug mode", action="store_true")
+    parser.add_argument("-o", "--outdir", help="Output directory", default="")
+    parser.add_argument("-p", "--outdir-prefix", help="Output directory prefix", default="uni-m-out")
+    args = parser.parse_args(argv)
+    conf = Config(args.cmd, args.query, args.debug)
+    conf.init()
+    conf.conf_files.set_out_dir(args.outdir, args.outdir_prefix)
+    return conf
+
 
 def select_from_meta(meta: dict, query: str) -> dict:
   def check_int(s: str) -> bool:
@@ -142,7 +291,7 @@ def run(root_dir: str, bug_info: dict, patchid: str, outdir: str, cmd: str, addi
     new_no = find_num(new_output_dir, "uni-cmp-out")
     new_snapshot_dir = os.path.join(new_output_dir, "snapshot")
     new_uni_out_dir = os.path.join(new_output_dir, f"uni-cmp-out-{new_no}")
-    NEW_UNI_KLEE_DEFAULT_OPTS = f"--patch-id={patch_ids} {link_opts} --make-lazy --make-all-symbolic --dump-states-on-halt=0 --output-dir={new_uni_out_dir} --start-from-snapshot --write-smt2s --write-kqueries --libc=uclibc --allocate-determ --posix-runtime --external-calls=all --no-exit-on-error --dump-snapshot --log-trace --simplify-sym-indices --target-function={target_function} --snapshot={snapshot_dir}/{snapshot_file}"
+    NEW_UNI_KLEE_DEFAULT_OPTS = f"--patch-id={patch_ids} {link_opts} --make-lazy --output-dir={new_uni_out_dir} --write-smt2s --write-kqueries --libc=uclibc --allocate-determ --posix-runtime --external-calls=all --no-exit-on-error --dump-snapshot --log-trace --simplify-sym-indices --target-function={target_function} --snapshot={snapshot_dir}/{snapshot_file}"
     execute(f"uni-klee {NEW_UNI_KLEE_DEFAULT_OPTS} {test_cmd}", data_dir)
 
 def filter_patch(root_dir: str, bug_info: dict):
@@ -205,7 +354,7 @@ def batch_cmp(root_dir: str, bug_info: dict, out_dir: str):
 
 
 def main(args: List[str]):
-  root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+  root_dir = ROOT_DIR
   os.chdir(root_dir)
   if len(args) < 3:
     print(f"Usage: {args[0]} <cmd> <query>")
